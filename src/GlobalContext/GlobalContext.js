9f9ad245d7d120ecc713a28a487871295fe3021f
Jahid Hasan <jahidruetian044@gmail.com>
2021-09-26 08:48:27 +0600

    data persistency done

--- src/GlobalContext/GlobalContext.js
+++ src/GlobalContext/GlobalContext.js
@@ -1,10 +1,14 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const globalContext = createContext([]);
 
 function GlobalContext(props) {
     const [modalCall, setModalCall] = useState(false);
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(()=>{return localStorage.getItem("cartItem")!==null?JSON.parse(localStorage.getItem("cartItem")):[]});
+    
+    useEffect(() => {
+        localStorage.setItem("cartItem", JSON.stringify(cart))
+    },["cartItem",cart])
     
     return (
         <globalContext.Provider value={{ modalCall, setModalCall, cart, setCart }}>
